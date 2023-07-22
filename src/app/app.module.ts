9ef72a8fe11065099d887c84bffdfaa7e989ad33
jomaosssss <joma.ostgo@gmail.com>
2023-07-22 09:30:02 -0600

    10componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
+import {MatCardModule} from '@angular/material/card';
+
+import {MatChipsModule} from '@angular/material/chips';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+
+import {MatSelectModule} from '@angular/material/select';
+
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+import {MatRadioModule} from '@angular/material/radio';
+
+import {MatBadgeModule} from '@angular/material/badge';
+
+import {MatListModule} from '@angular/material/list';
+
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -10,7 +38,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatBadgeModule,
+    MatListModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
